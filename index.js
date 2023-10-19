fd4c2369f5f92d1b6ac4313084ebe9666049833a
Tony Eder <tonyeder11@gmail.com>
2023-10-19 05:51:40 -0700

    passed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(array) {
+    let winningYear = array.find(function(object){
+      if(object.result === "W") {
+        return true
+      } else {}
+    }) 
+    if(winningYear === undefined) {
+      return undefined
+    }
+        return winningYear.year
+    }
+
+
